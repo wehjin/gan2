037b5dae72b248141da5f5faf66ef3e67ca15f49
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-10-01 15:40:11 -0700

    Move common js into separate files

--- ch12/d2/beats.js
+++ ch12/d2/beats.js
@@ -125,32 +125,3 @@ const beats = [
         "phrase": 27,
     }
 ];
-
-function addBeat(beat, host) {
-    let phrase = beat["phrase"];
-    let id = "sentence" + phrase;
-    let p = document.createElement("p");
-    p.id = id;
-    {
-        let span = document.createElement("span");
-        span.className = "speaker";
-        span.appendChild(document.createTextNode(beat["speaker"] + ": "));
-        p.appendChild(span);
-    }
-    {
-        let trigger = document.createElement("button");
-        trigger.href = "#" + id;
-        trigger.onclick = function () {
-            play(phrase)
-        };
-        trigger.appendChild(document.createTextNode(beat["kana"]));
-        p.appendChild(trigger);
-    }
-    host.appendChild(p);
-}
-
-function addBeats(host) {
-    beats.forEach(function (beat) {
-        addBeat(beat, host)
-    })
-}
