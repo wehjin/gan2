5f7558c95bda1816512208396b62c52a3aaa7a39
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-10-01 14:21:01 -0700

    Move beats in separate file

---
+++ ch12/d2/beats.js
@@ -0,0 +1,156 @@
+const beats = [
+    {
+        "kana": "日本ではなつ　ぼんおどりがあります。",
+        "speaker": "narrator",
+        "phrase": 2,
+    },
+    {
+        "kana": "大人も子どもも　ゆかたを着て、おどります。",
+        "speaker": "narrator",
+        "phrase": 3,
+    },
+    {
+        "kana": "今日リンさんははじめて　ぼんおどりに行きます。",
+        "speaker": "narrator",
+        "phrase": 4,
+    },
+    {
+        "kana": "ねえ、リン。",
+        "speaker": "かおる",
+        "phrase": 6,
+    },
+    {
+        "kana": "今晩、うちの近くの神社でぼんおどりがあるけど、見に来ない？",
+        "speaker": "かおる",
+        "phrase": 7,
+    },
+    {
+        "kana": "えっ。",
+        "speaker": "リン",
+        "phrase": 8,
+    },
+    {
+        "kana": "ぼんおどり？",
+        "speaker": "リン",
+        "phrase": 9,
+    },
+    {
+        "kana": "それ、何？",
+        "speaker": "リン",
+        "phrase": 10,
+    },
+    {
+        "kana": "おもしろい？",
+        "speaker": "リン",
+        "phrase": 11,
+    },
+    {
+        "kana": "うん、とっても。",
+        "speaker": "かおる",
+        "phrase": 12,
+    },
+    {
+        "kana": "みなゆかたを着て、わになって、おどるの。",
+        "speaker": "かおる",
+        "phrase": 13,
+    },
+    {
+        "kana": "じょうずな人がたいこをたたいて、",
+        "speaker": "かおる",
+        "phrase": 14,
+    },
+    {
+        "kana": "とてもにぎやかで、楽しいわよ。",
+        "speaker": "かおる",
+        "phrase": 15,
+    },
+    {
+        "kana": "え、そう。",
+        "speaker": "リン",
+        "phrase": 16,
+    },
+    {
+        "kana": "よくわからないけど、見てみたい。",
+        "speaker": "リン",
+        "phrase": 17,
+    },
+    {
+        "kana": "それに、いろいろなろ店もあって、",
+        "speaker": "かおる",
+        "phrase": 18,
+    },
+    {
+        "kana": "たこやきとかわたがしとか売っているの。",
+        "speaker": "かおる",
+        "phrase": 19,
+    },
+    {
+        "kana": "わあ、行ってみたい。",
+        "speaker": "リン",
+        "phrase": 20,
+    },
+    {
+        "kana": "私もおどることができるかな。",
+        "speaker": "リン",
+        "phrase": 21,
+    },
+    {
+        "kana": "もちろん。",
+        "speaker": "かおる",
+        "phrase": 22,
+    },
+    {
+        "kana": "おどりはかんだんよ。",
+        "speaker": "かおる",
+        "phrase": 23,
+    },
+    {
+        "kana": "でも、私、ゆかた持ってないけど。",
+        "speaker": "リン",
+        "phrase": 24,
+    },
+    {
+        "kana": "ようふくじゃいけない？",
+        "speaker": "リン",
+        "phrase": 25,
+    },
+    {
+        "kana": "ううん、ようふくでも大じょうぶ。",
+        "speaker": "かおる",
+        "phrase": 26,
+    },
+    {
+        "kana": "そう、じゃ、いく。",
+        "speaker": "リン",
+        "phrase": 27,
+    }
+];
+
+function addBeat(beat, host) {
+    let phrase = beat["phrase"];
+    let id = "sentence" + phrase;
+    let p = document.createElement("p");
+    p.id = id;
+    {
+        let span = document.createElement("span");
+        span.className = "speaker";
+        span.appendChild(document.createTextNode(beat["speaker"] + ": "));
+        p.appendChild(span);
+    }
+    {
+        let a = document.createElement("a");
+        a.href = "#" + id;
+        a.onclick = function () {
+            play(phrase)
+        };
+        a.appendChild(document.createTextNode(beat["kana"]));
+        p.appendChild(a);
+    }
+    host.appendChild(p);
+}
+
+function addBeats(host) {
+    beats.forEach(function (beat) {
+        addBeat(beat, host)
+    })
+}
