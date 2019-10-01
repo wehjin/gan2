dce823dfc0d91520d7cca31a7a15481583b65866
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-09-30 17:41:07 -0700

    Add phrases database.

---
+++ ch12/d2/phrases.js
@@ -0,0 +1,261 @@
+const phrases = [
+    {
+        "href": "#sentence000",
+        "onclick": "play(0, 2.53286, 3.59705)",
+        "index": 0,
+        "start": 2.533,
+        "end": 3.597,
+        "duration": 1.064
+    },
+    {
+        "href": "#sentence001",
+        "onclick": "play(1, 4.06603, 6.758590000000001)",
+        "index": 1,
+        "start": 4.066,
+        "end": 6.759,
+        "duration": 2.693
+    },
+    {
+        "href": "#sentence002",
+        "onclick": "play(2, 7.28977, 8.26327)",
+        "index": 2,
+        "start": 7.29,
+        "end": 8.263,
+        "duration": 0.974
+    },
+    {
+        "href": "#sentence003",
+        "onclick": "play(3, 8.63961, 9.71916)",
+        "index": 3,
+        "start": 8.64,
+        "end": 9.719,
+        "duration": 1.08
+    },
+    {
+        "href": "#sentence004",
+        "onclick": "play(4, 10.434199999999999, 11.8309)",
+        "index": 4,
+        "start": 10.434,
+        "end": 11.831,
+        "duration": 1.397
+    },
+    {
+        "href": "#sentence005",
+        "onclick": "play(5, 12.426, 17.3947)",
+        "index": 5,
+        "start": 12.426,
+        "end": 17.395,
+        "duration": 4.969
+    },
+    {
+        "href": "#sentence006",
+        "onclick": "play(6, 19.538500000000003, 22.876199999999997)",
+        "index": 6,
+        "start": 19.539,
+        "end": 22.876,
+        "duration": 3.338
+    },
+    {
+        "href": "#sentence007",
+        "onclick": "play(7, 23.9224, 25.6834)",
+        "index": 7,
+        "start": 23.922,
+        "end": 25.683,
+        "duration": 1.761
+    },
+    {
+        "href": "#sentence008",
+        "onclick": "play(8, 25.9813, 28.591299999999997)",
+        "index": 8,
+        "start": 25.981,
+        "end": 28.591,
+        "duration": 2.61
+    },
+    {
+        "href": "#sentence009",
+        "onclick": "play(9, 28.8598, 31.216299999999997)",
+        "index": 9,
+        "start": 28.86,
+        "end": 31.216,
+        "duration": 2.356
+    },
+    {
+        "href": "#sentence010",
+        "onclick": "play(10, 31.7451, 32.2468)",
+        "index": 10,
+        "start": 31.745,
+        "end": 32.247,
+        "duration": 0.502
+    },
+    {
+        "href": "#sentence011",
+        "onclick": "play(11, 32.4941, 33.7801)",
+        "index": 11,
+        "start": 32.494,
+        "end": 33.78,
+        "duration": 1.286
+    },
+    {
+        "href": "#sentence012",
+        "onclick": "play(12, 34.047000000000004, 34.9732)",
+        "index": 12,
+        "start": 34.047,
+        "end": 34.973,
+        "duration": 0.926
+    },
+    {
+        "href": "#sentence013",
+        "onclick": "play(13, 35.2411, 35.8458)",
+        "index": 13,
+        "start": 35.241,
+        "end": 35.846,
+        "duration": 0.605
+    },
+    {
+        "href": "#sentence014",
+        "onclick": "play(14, 36.0416, 37.5557)",
+        "index": 14,
+        "start": 36.042,
+        "end": 37.556,
+        "duration": 1.514
+    },
+    {
+        "href": "#sentence015",
+        "onclick": "play(15, 38.0011, 39.7343)",
+        "index": 15,
+        "start": 38.001,
+        "end": 39.734,
+        "duration": 1.733
+    },
+    {
+        "href": "#sentence016",
+        "onclick": "play(16, 39.9645, 42.2149)",
+        "index": 16,
+        "start": 39.965,
+        "end": 42.215,
+        "duration": 2.25
+    },
+    {
+        "href": "#sentence017",
+        "onclick": "play(17, 43.9142, 49.7709)",
+        "index": 17,
+        "start": 43.914,
+        "end": 49.771,
+        "duration": 5.857
+    },
+    {
+        "href": "#sentence018",
+        "onclick": "play(18, 50.3786, 53.0068)",
+        "index": 18,
+        "start": 50.379,
+        "end": 53.007,
+        "duration": 2.628
+    },
+    {
+        "href": "#sentence019",
+        "onclick": "play(19, 53.8188, 55.9026)",
+        "index": 19,
+        "start": 53.819,
+        "end": 55.903,
+        "duration": 2.084
+    },
+    {
+        "href": "#sentence020",
+        "onclick": "play(20, 56.2276, 58.4021)",
+        "index": 20,
+        "start": 56.228,
+        "end": 58.402,
+        "duration": 2.174
+    },
+    {
+        "href": "#sentence021",
+        "onclick": "play(21, 59.095, 61.635799999999996)",
+        "index": 21,
+        "start": 59.095,
+        "end": 61.636,
+        "duration": 2.541
+    },
+    {
+        "href": "#sentence022",
+        "onclick": "play(22, 62.130500000000005, 64.92760000000001)",
+        "index": 22,
+        "start": 62.131,
+        "end": 64.928,
+        "duration": 2.797
+    },
+    {
+        "href": "#sentence023",
+        "onclick": "play(23, 65.21279999999999, 66.1254)",
+        "index": 23,
+        "start": 65.213,
+        "end": 66.125,
+        "duration": 0.913
+    },
+    {
+        "href": "#sentence024",
+        "onclick": "play(24, 66.32759999999999, 68.11110000000001)",
+        "index": 24,
+        "start": 66.328,
+        "end": 68.111,
+        "duration": 1.784
+    },
+    {
+        "href": "#sentence025",
+        "onclick": "play(25, 68.5064, 72.11160000000001)",
+        "index": 25,
+        "start": 68.506,
+        "end": 72.112,
+        "duration": 3.605
+    },
+    {
+        "href": "#sentence026",
+        "onclick": "play(26, 72.62129999999999, 75.3527)",
+        "index": 26,
+        "start": 72.621,
+        "end": 75.353,
+        "duration": 2.731
+    },
+    {
+        "href": "#sentence027",
+        "onclick": "play(27, 76.79929999999999, 78.84190000000001)",
+        "index": 27,
+        "start": 76.799,
+        "end": 78.842,
+        "duration": 2.043
+    },
+    {
+        "href": "#sentence028",
+        "onclick": "play(28, 79.5859, 82.8747)",
+        "index": 28,
+        "start": 79.586,
+        "end": 82.875,
+        "duration": 3.289
+    },
+    {
+        "href": "#sentence029",
+        "onclick": "play(29, 83.0966, 84.2012)",
+        "index": 29,
+        "start": 83.097,
+        "end": 84.201,
+        "duration": 1.105
+    }
+];
+
+const player = document.getElementById('player');
+
+function play(index) {
+    let phrase = phrases[index];
+    let startTime = phrase["start"];
+    let endTime = phrase["end"];
+    player.pause();
+    player.currentTime = startTime;
+    player.ontimeupdate = function () {
+        let time = player.currentTime;
+        if (time > endTime) {
+            player.pause();
+            player.currentTime = startTime;
+        }
+    };
+    player.play();
+    console.log('play', index);
+}
